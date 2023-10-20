82e7d9e6d189563eba08ac969a74d9010e02be85
crazydevlegend <crazy010323@gmail.com>
2023-10-20 10:17:04 -0700

    remove vite-plugin-rewrite

--- packages/stablestudio-ui/vite.config.ts
+++ packages/stablestudio-ui/vite.config.ts
@@ -3,8 +3,6 @@ import * as ChildProcess from "child_process";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { Express } from 'express';
-import Rewrite from 'vite-plugin-rewrite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
